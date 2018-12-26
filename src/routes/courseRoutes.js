40a304c3d3fe5572ca6da84482aaf2ce0d9baf1f
sradms0 <sradms0@openmailbox.org>
2018-12-25 19:45:57 -0500

    add individual course route (by id)

--- src/routes/courseRoutes.js
+++ src/routes/courseRoutes.js
@@ -4,4 +4,7 @@ module.exports = app => {
     const controller = require('../controllers/courseController');
     app.route('/api/courses')
         .get(controller.getCourses)     // GET  courses
+
+    app.route('/api/courses/:id')
+        .get(controller.getCourse)     // GET  course
 };
