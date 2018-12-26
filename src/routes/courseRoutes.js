a4daec993403f5bd2b70fbb0462d97e2f254a695
sradms0 <sradms0@openmailbox.org>
2018-12-25 19:48:15 -0500

    add createCourse route

--- src/routes/courseRoutes.js
+++ src/routes/courseRoutes.js
@@ -4,6 +4,7 @@ module.exports = app => {
     const controller = require('../controllers/courseController');
     app.route('/api/courses')
         .get(controller.getCourses)     // GET  courses
+        .post(controller.createCourse) // POST  course
 
     app.route('/api/courses/:id')
         .get(controller.getCourse)     // GET  course
