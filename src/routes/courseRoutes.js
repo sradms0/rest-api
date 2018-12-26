b9eecba4cbd38d0df624116bba5a3da9ad723d30
sradms0 <sradms0@openmailbox.org>
2018-12-26 12:09:18 -0500

    add createReview route

--- src/routes/courseRoutes.js
+++ src/routes/courseRoutes.js
@@ -3,10 +3,13 @@
 module.exports = app => {
     const controller = require('../controllers/courseController');
     app.route('/api/courses')
-        .get(controller.getCourses)     // GET  courses
-        .post(controller.createCourse) // POST  course
+        .get(controller.getCourses)                 // GET  courses
+        .post(controller.createCourse);             // POST course
 
     app.route('/api/courses/:id')
-        .get(controller.getCourse)     // GET  course
-        .put(controller.updateCourse); // PUT  course
+        .get(controller.getCourse)                  // GET  course
+        .put(controller.updateCourse);              // PUT  course
+
+    app.route('/api/courses/:courseId/reviews')
+        .post(controller.createReview)              // POST review
 };
