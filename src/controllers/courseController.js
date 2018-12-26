8dfb490d4f6b52b29af462cbe182cc48493142fd
sradms0 <sradms0@openmailbox.org>
2018-12-25 19:47:11 -0500

    add createCourse

--- src/controllers/courseController.js
+++ src/controllers/courseController.js
@@ -29,3 +29,36 @@ exports.getCourse = (req, res, next) => {
         return res.json(course);
     });
 };
+exports.createCourse = (req, res, next) => {
+    authenticate(req, res, next)
+    .then(user => {
+        if (req.body.title          &&
+            req.body.description    &&
+            req.body.steps
+        ) {
+            // create object with form input
+            const courseData = {
+                title:          req.body.title,
+                description:    req.body.description,
+                user:           user._id,
+                steps:          req.body.steps
+            };
+
+            // use schema's `create` method to insert document into Mongo
+            Course.create(courseData, (error, course) => {
+                if (error) {
+                    const err = new Error(error.message);
+                    err.status = 400;
+                    return next(err);
+                }
+                res.status(201);
+                res.setHeader('Location','/');
+                res.end();
+            });
+        } else {
+            const err = new Error('required fields: title, description, user, steps');
+            err.status = 400;
+            return next(err);
+        }
+    });
+};
