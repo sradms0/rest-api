3db8cb39d4fbad19a9f6dd54af255ae54fc7c757
sradms0 <sradms0@openmailbox.org>
2018-12-26 12:08:25 -0500

    add createReview

--- src/controllers/courseController.js
+++ src/controllers/courseController.js
@@ -5,6 +5,7 @@ const {authenticate}    = require('../controllers/userController');
 
 const User   = mongoose.model('User');
 const Course = mongoose.model('Course');
+const Review = mongoose.model('Review');
 
 exports.getCourses = (req, res, next) => {
     authenticate(req, res, next)
@@ -75,6 +76,37 @@ exports.updateCourse = (req, res, next) => {
             }
             res.status(204);
             res.end();
+        });
+    });
+};
+exports.createReview = (req, res, next) => {
+    const check = (error, next) => {
+        if (error) {
+            const err = new Error(error.message);
+            err.status = 400;
+            return next(err);
+        }
+    };
+    authenticate(req, res, next)
+    .then(user => {
+        // find course to review
+        Course.findById(req.params.courseId)
+        .exec((error, course) => {
+            check(error, next);
+
+            // create the review
+            Review.create(req.body, (error, review) => {
+                check(error, next);
+
+                // add it, and save
+                course.reviews.push(review);
+                course.save(error => {
+                    check(error, next);
+                    res.setHeader('Location', `/api/courses/${req.params.courseId}`);
+                    res.status(201);
+                    res.end();
+                });
+            });
         });
     });
 };
