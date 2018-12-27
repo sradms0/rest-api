b8b0bed640c1a32a576a9f42127291892e921abd
sradms0 <sradms0@openmailbox.org>
2018-12-26 22:17:45 -0500

    return all courses rather than courses taught only by current user

--- src/controllers/courseController.js
+++ src/controllers/courseController.js
@@ -19,10 +19,7 @@ exports.getCourses = (req, res, next) => {
     authenticate(req, res, next)
     .then(user => {
         // display only _id and title
-        Course.find(
-            {user: user._id}, 
-            {_id: true, title: true}
-        )
+        Course.find({}, {_id: true, title: true})
         .exec((error, courses) => {
             if (error) return next(error);
             return res.json(courses);
